0c061862932232b4f7de11379ff5044cc5a1e3f4
Yousry Essam <youssry.essam2111@gmail.com>
2023-04-21 23:00:05 +0200

    Model 3 by Yousry

--- project SW/Models/Actor_Movie.cs
+++ project SW/Models/Actor_Movie.cs
@@ -2,5 +2,9 @@
 {
     public class Actor_Movie
     {
+        public int ActorID { get; set; }
+        public Movie Movie { get; set; }
+        public int MovieID { get; set; }
+        public Actor Actor { get; set; }
     }
 }
--- project SW/Startup.cs
+++ project SW/Startup.cs
@@ -1,10 +1,25 @@
-﻿using Microsoft.AspNetCore.Authentication.Cookies;
+﻿//using project_SW.Data;
+//using project_SW.Data.Cart;
+//using project_SW.Data.Services;
+using project_SW.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using project_SW.Data;
-using System.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace project_SW
+namespace eTickets
 {
     public class Startup
     {
